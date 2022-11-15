249eb1b7ed10ba8f4997295d7430e2e7b8f1615f
ErikNissen <73883328+ErikNissen@users.noreply.github.com>
2022-11-15 15:27:25 +0100

    init

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+/*
+Autor:          Erik Nissen
+Studiengang:    Informationstechnologie (INI)
+Matrikelnummer: 937388
+Datei:          main.cpp
+Erstellt:       15.11.2022
+*/
+
+#include "main.hpp"
---
+++ src/main.hpp
@@ -0,0 +1,19 @@
+/*
+Autor:          Erik Nissen
+Studiengang:    Informationstechnologie (INI)
+Matrikelnummer: 937388
+Datei:          main.hpp
+Erstellt:       15.11.2022
+*/
+
+
+#ifndef NP_SEMESTERPROJEKT_MAIN_HPP
+#define NP_SEMESTERPROJEKT_MAIN_HPP
+
+
+class main {
+
+};
+
+
+#endif //NP_SEMESTERPROJEKT_MAIN_HPP
