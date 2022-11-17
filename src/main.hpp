e9877438b6e2396907dc5f2dd9a21c6858888bae
ErikNissen <73883328+ErikNissen@users.noreply.github.com>
2022-11-17 17:24:49 +0100

    Update main.hpp

--- src/main.hpp
+++ src/main.hpp
@@ -1,12 +1,3 @@
-/*
-Autor:          Erik Nissen
-Studiengang:    Informationstechnologie (INI)
-Matrikelnummer: 937388
-Datei:          main.hpp
-Erstellt:       15.11.2022
-*/
-
-
 #ifndef NP_SEMESTERPROJEKT_MAIN_HPP
 #define NP_SEMESTERPROJEKT_MAIN_HPP
 
