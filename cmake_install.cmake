decf61806772754fb49124cc436e9dc48a59b423
KS <85065292+UnknownUserAccount@users.noreply.github.com>
2022-12-12 16:14:29 +0100

    Merged Inventory to Dev

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -42,6 +42,11 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("C:/Users/Victor Rodriguez/CLionProjects/NP-Semesterprojekt/src/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/Victor Rodriguez/CLionProjects/NP-Semesterprojekt/_deps/json-build/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
