9d006c7f83144867490102c9588d1ac32f276a04
KurayamiShadow <nico.johnsen@hotmail.com>
2022-12-12 16:25:50 +0100

    fix Cmake Lib PersistentFileManagment

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Victor Rodriguez/CLionProjects/NP-Semesterprojekt
+# Install script for directory: F:/Programmierungs Sachen/C & C++/N+P/NP-Semesterprojekt
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,12 +34,17 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/msys64/mingw64/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "H:/CLion/CLion 2021.3.4/bin/mingw/bin/objdump.exe")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/Victor Rodriguez/CLionProjects/NP-Semesterprojekt/src/cmake_install.cmake")
+  include("F:/Programmierungs Sachen/C & C++/N+P/NP-Semesterprojekt/src/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("F:/Programmierungs Sachen/C & C++/N+P/NP-Semesterprojekt/_deps/json-build/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -50,5 +55,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Victor Rodriguez/CLionProjects/NP-Semesterprojekt/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "F:/Programmierungs Sachen/C & C++/N+P/NP-Semesterprojekt/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
